0dfa0953e976902020eb2c823f40166bf42bf2bb
ka-zuumi <39542433+ka-zuumi@users.noreply.github.com>
2022-01-12 08:16:09 -1000

    Add files via upload

---
+++ lookAtVesicle.tcl
@@ -0,0 +1,31 @@
+axes location Off
+color Display Background gray
+color change rgb gray 0.750000 0.750000 0.750000
+
+mol modstyle 0 0 Points 12.000000
+mol modselect 0 0 resname CHL1
+mol modcolor 0 0 ColorID 4
+mol color ColorID 4
+mol representation Points 12.000000
+mol selection resname CHL1
+mol material Opaque
+
+mol addrep 0
+mol modselect 1 0 resname POPC
+mol modcolor 1 0 ColorID 1
+mol color ColorID 1
+mol representation Points 12.000000
+mol selection resname POPC
+mol material Opaque
+
+mol addrep 0
+mol modselect 2 0 resname POPE
+mol modcolor 2 0 ColorID 0
+mol color ColorID 0
+mol representation Points 12.000000
+mol selection resname POPE
+mol material Opaque
+
+mol addrep 0
+mol modselect 3 0 resname POPS
+mol modcolor 3 0 ColorID 7
