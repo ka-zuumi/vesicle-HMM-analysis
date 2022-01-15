03a595aa270d1fe179442419160fbcd991f24d3c
ka-zuumi <39542433+ka-zuumi@users.noreply.github.com>
2022-01-14 21:54:52 -1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HMM Vesicle Analysis'
+copyright = '2022, Fujioka'
+author = 'Fujioka'
 
 release = '0.1'
 version = '0.1.0'
