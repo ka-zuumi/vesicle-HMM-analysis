ac8a1799e88a1b77f463171f3dbd09c6f311a647
ka-zuumi <39542433+ka-zuumi@users.noreply.github.com>
2022-01-16 12:09:19 -1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,14 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
+
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
